bfb94e1550b0cea92ced291845951bc945275b61
Thomas <145203049+meetThomas@users.noreply.github.com>
2023-11-29 12:10:03 +0530

    Add files via upload

---
+++ ThreadDemo.java
@@ -0,0 +1,54 @@
+package Java;
+import java.util.Scanner;
+
+
+class MultiplicationTable{
+//	void PrintMultiplicationTable(int number) {
+		synchronized void PrintMultiplicationTable(int number) {
+		for(int i =1;i<=10;i++) {
+			System.out.println(i+"X"+number+"="+i*number);
+		}
+	}
+}
+
+
+class MyThread1 extends Thread{
+	MultiplicationTable t ;
+	MyThread1(MultiplicationTable t){
+	this.t=t;
+	
+}
+	public void run(){
+		t.PrintMultiplicationTable(5);
+	}
+}
+	 
+	class MyThread2 extends Thread{
+		MultiplicationTable t ;
+		MyThread2(MultiplicationTable t){
+		this.t=t;
+		}
+		
+		public void run(){
+			t.PrintMultiplicationTable(7);
+		}
+	}
+	
+	
+public class ThreadDemo {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+	MultiplicationTable m = new MultiplicationTable();
+    MyThread1 t=new MyThread1(m);
+   
+	
+    MyThread2 t2=new MyThread2(m); 
+    t.start();
+   
+    t2.start();
+    
+	}
+
+
+}
